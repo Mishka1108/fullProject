572ca44c28014335f8abdab4dccf6bd913491be4
Mishka1108 <mr.mishka3003@gmail.com>
2025-08-13 01:22:53 -0700

    express

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const rateLimit = require('express-rate-limit');
 
 // Environment configuration
 dotenv.config();
